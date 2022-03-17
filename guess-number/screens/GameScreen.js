ec94739119e9565be1716712a3b62437e69138d5
varun.chandwani@accenture.com <varun.chandwani@accenture.com>
2022-03-17 15:41:42 +1100

    Dynamic CSS changes
    Dynamic CSS changes

--- guess-number/screens/GameScreen.js
+++ guess-number/screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Alert,
   ScrollView,
   FlatList,
+  Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -112,13 +113,13 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
   },
   listContainer: {
     flex: 1,
-    width: "60%",
+    width: Dimensions.get("window").width > 350 ? "60%" : "80%",
   },
   list: {
     flexGrow: 1,

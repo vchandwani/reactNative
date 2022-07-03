9f9f6c0c34f26b9b269a2953d0ebbceeeda18624
varunchandwani <varunchandwani@gmail.com>
2022-07-03 14:09:03 +1000

    IOS Shadow

--- mini-game/screens/StartGameScreen.js
+++ mini-game/screens/StartGameScreen.js
@@ -21,5 +21,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#72063c",
     borderRadius: 8,
     elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
 });

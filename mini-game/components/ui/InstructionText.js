9e3587792009b137d789d7d3a396dee321f3b7c8
vchandwani <varunchandwani@gmail.com>
2022-07-09 17:38:56 +1000

    Update InstructionText.js

--- mini-game/components/ui/InstructionText.js
+++ mini-game/components/ui/InstructionText.js
@@ -2,7 +2,7 @@ import { Text, StyleSheet } from "react-native";
 import Colors from "../../constants/colors";
 
 function InstructionText({ children, style }) {
-  return <Text style={[styles.instructionText, style]}>{{ children }}</Text>;
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 export default InstructionText;

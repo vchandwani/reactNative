ee63ee70c019ba52559f1ffb7c8071f4fa70ae90
vchandwani <varunchandwani@gmail.com>
2022-07-12 11:32:37 +1000

    CSS added
    CSS added

--- meals-app/components/CategoryGridTitle.js
+++ meals-app/components/CategoryGridTitle.js
@@ -1,11 +1,18 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, Platform } from "react-native";
+import { StyleSheet } from "react-native";
 
 function CategoryGridTitle({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={[styles.gridItem]}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.buttonStyle,
+          pressed && styles.buttonPressed,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +20,36 @@ function CategoryGridTitle({ title, color }) {
 }
 
 export default CategoryGridTitle;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  buttonStyle: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});

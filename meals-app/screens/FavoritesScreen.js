a7c110ea6a8cd69381590157e4be9beb1090d21c
vchandwani <varunchandwani@gmail.com>
2022-07-14 13:54:12 +1000

    Content Added
    Content Added

--- meals-app/screens/FavoritesScreen.js
+++ meals-app/screens/FavoritesScreen.js
@@ -1,7 +1,36 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { useContext } from "react";
+
+import MealsList from "../components/MealsList/MealsList";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+  const favoritesContext = useContext(FavoritesContext);
+  const favMeals = MEALS.filter((meal) =>
+    favoritesContext.ids.includes(meal.id)
+  );
+  if (favMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>YOu have no favorite meals</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favMeals} />;
 }
 
 export default FavoritesScreen;
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "bold",
+  },
+});

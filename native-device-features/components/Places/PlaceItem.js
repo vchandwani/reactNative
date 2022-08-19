5530835b5fc31a420a3c1907b3dd4fd67dcd3324
vchandwani <varunchandwani@gmail.com>
2022-08-19 13:18:07 +1000

    New Screens added with Components
    New Screens added with Components

---
+++ native-device-features/components/Places/PlaceItem.js
@@ -0,0 +1,22 @@
+import { View, Text, Image, Pressable } from "react-native";
+
+function PlaceItem({ place }) {
+  return (
+    <Pressable onPress={onSelect}>
+      <View>
+        <Image source={{ uri: place.imageUri }} />
+        <View>
+          <Text>{place.title}</Text>
+          <Text>{place.address}</Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+}
+
+export default PlaceItem;
+
+const styles = StyleSheet.create({
+  title: {},
+  address: {},
+});
---
+++ native-device-features/components/Places/PlacesList.js
@@ -0,0 +1,32 @@
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import PlaceItem from "./PlaceItem";
+
+function PlacesList({ places }) {
+  if (!places || place.length === 0) {
+    return (
+      <View style={styles.fallBackContainer}>
+        <Text style={styles.fallBackText}>No Places added yet</Text>
+      </View>
+    );
+  }
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(item) => <PlaceItem place={item} />}
+    />
+  );
+}
+
+export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallBackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  fallBackText: {
+    fontSize: 16,
+  },
+});

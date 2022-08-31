38a080571696b09896b40cf33747197264bf42a2
vchandwani <varunchandwani@gmail.com>
2022-08-31 16:50:27 +1000

    Outlined Button added
    Outlined Button added

--- native-device-features/components/Places/ImagePicker.js
+++ native-device-features/components/Places/ImagePicker.js
@@ -6,6 +6,7 @@ import {
 } from "expo-image-picker";
 import { useState } from "react";
 import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
   const [pickedImage, setPickedImage] = useState();
@@ -52,7 +53,7 @@ function ImagePicker() {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <OutlinedButton onPress={takeImageHandler}>Take Image</OutlinedButton>
     </View>
   );
 }

6c96c4edf8e5359922eb6b4e54087414eefe944a
vchandwani <varunchandwani@gmail.com>
2022-09-06 17:23:57 +1000

    Place details added
    Place details added

--- native-device-features/App.js
+++ native-device-features/App.js
@@ -10,6 +10,7 @@ import Map from "./screens/Map";
 import { init } from "./util/database";
 import { useEffect, useState } from "react";
 import AppLoading from "expo-app-loading";
+import PlaceDetails from "./screens/PlaceDetails";
 
 const Stack = createNativeStackNavigator();
 
@@ -62,6 +63,11 @@ export default function App() {
             }}
           />
           <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen
+            name="PlaceDetails"
+            component={PlaceDetails}
+            options={{ title: "Loading Place..." }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>

affac9b657b233641d4eb03a40280d3320503f30
vchandwani <varunchandwani@gmail.com>
2022-09-06 16:38:25 +1000

    Database Fetch for Places
    Database Fetch for Places

--- native-device-features/App.js
+++ native-device-features/App.js
@@ -7,10 +7,27 @@ import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
 import { Colors } from "./constants/colors";
 import Map from "./screens/Map";
+import { init } from "./util/database";
+import { useEffect, useState } from "react";
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  if (!dbInitialized) {
+    return <AppLoading />;
+  }
   return (
     <>
       <StatusBar style="dark" />
